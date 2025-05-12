eeeb0ee381935ab32e9b92972481b8e99d410a3e
beardm4n <dmitriykorovko@gmail.com>
2025-05-12 23:58:11 +0300

    add about page

--- app.vue
+++ app.vue
@@ -6,11 +6,13 @@
         title="Профессиональное управление инвестициями"
         sub-title="Ваш надежный партнер в мире финансов"
     />
+    <AboutUs />
   </div>
 </template>
 
 <script setup>
 import ImageWithText from '~/components/layouts/ImageWithText.vue';
+import AboutUs from '~/components/layouts/AboutUs.vue';
 import { useAssetUrl } from '~/composables/useAssetUrl';
 
 const img = useAssetUrl('images/main_img.png');
